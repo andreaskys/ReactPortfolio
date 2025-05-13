714e63f0d32d7de58e111f46c5025452c6ef844f
andreas <andreasnunesmachado2016@gmail.com>
2025-05-13 00:40:14 -0300

    final commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,33 @@
 import {ThemeToggle} from "@/components/ThemeToggle.jsx";
 import {StarBackground} from "@/components/StarBackground"
+import {Navbar} from "@/components/Navbar.jsx";
+import {HeroSection} from "@/components/HeroSection.jsx";
+import {AboutSection} from "@/components/AboutSection.jsx";
+import {SkillsSection} from "@/components/SkillsSection.jsx";
+import {ProjectsSection} from "@/components/ProjectsSection.jsx";
+import {ContactSection} from "@/components/ContactSection.jsx";
+import {Footer} from "@/components/Footer.jsx";
 
 export const Home = () => {
     return(
         <div className={"min-h-screen bg-backgorund text-foreground overflow-x-hidden"}>
 
             {/*Theme Toggle*/}
-            <ThemeToggle/>
+            <ThemeToggle />
             {/*background effects*/}
-            <StarBackground/>
+            <StarBackground />
             {/*Navbar*/}
-
+            <Navbar />
             {/*Main Content*/}
-
+            <main>
+               <HeroSection />
+                <AboutSection />
+                <SkillsSection />
+                <ProjectsSection />
+                <ContactSection />
+            </main>
             {/*Footer*/}
+            <Footer/>
         </div>
     );
 };
